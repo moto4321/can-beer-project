57ed4605d3a90c030f2923b67cab359428b40a7b
yogoloper <jguddnjs@gmail.com>
2022-03-07 16:07:57 +0900

    기본 페에지 구성

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print('bear can!')
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
+
+client = MongoClient('mongodb+srv://test:sparta@cluster0.g33mv.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
