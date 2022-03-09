e4fcab0990e31095ec0d658b73439a0ff460c72a
moto4321 <moto432195@gmail.com>
2022-03-09 21:09:33 +0900

    update post

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
+import random
+
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 app = Flask(__name__)
 import os
 from pymongo import MongoClient
 import certifi
 from datetime import datetime, timedelta # 파일명 생성을 위한 datetime 라이브러리 사용
+#from datetime import datetime # 파일명 생성을 위한 datetime 라이브러리 사용
+
 ca = certifi.where()
 #client = MongoClient('localhost', 27017)
 client = MongoClient('mongodb+srv://test:sparta@cluster0.g33mv.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
@@ -16,23 +20,31 @@ import hashlib
 # 메인 페이지
 @app.route('/')
 def home():
+    # 토큰 확인
+    token_receive = request.cookies.get('mytoken')
+    isLogin = False
+    # 토큰이 있으면 로그인 플래그 설정
+    if token_receive is not None:
+        isLogin = True
+
     # 맥주 리스트 조회
     content_list = list(db.content.find({}, {'_id': False}))
 
     # 맥주별 최저가를 구해서 맥주 리스트에 추가
     for row in content_list:
-        if 'price' in row:
-            row['one_min'] = format((min(row['price'], key=(lambda x: x['one'])))['one'], ',')
-            row['four_min'] = format((min(row['price'], key=(lambda x: x['four'])))['four'], ',')
+        row['one_min'] = (min(row['price'], key=(lambda x: x['one'])))['one']
+        row['four_min'] = (min(row['price'], key=(lambda x: x['four'])))['four']
 
         # 맥주별 리뷰를 조회해서 별점 평균값을 맥주 리스트에 추가
-        review_list = list(db.review.find({'beer_num':row['beer_num']}, {'_id': False}))
+        review_list = list(db.review.find({'beer_num': row['beer_num']}, {'_id': False}))
         sum_star = 0
         if len(review_list) != 0:
             for review_row in review_list:
                 sum_star += review_row['star']
 
             row['star_point'] = round(sum_star / len(review_list), 1)
+        else:
+            row['star_point'] = 0
 
         # 맥주 출시일과 오늘간 날짜를 비교하여 신상품 여부를 맥주 리스트에 추가
         beer_date = datetime.strptime(row['beer_date'], '%Y-%m-%d')
@@ -42,10 +54,58 @@ def home():
         else:
             row['new_beer'] = False
 
-    return render_template('index.html', content_list=content_list)
+    # 정렬 타입 확인하여
+    if 'align_type' in request.args:
+        align_type = int(request.args.get('align_type'))
+    else:
+        align_type = 0
+
+    # 맥주 리스트 정렬
+    if align_type == 0: # 기본 정렬
+        content_list = content_list
+    elif align_type == 1:   # 최근 상품순
+        content_list = sorted(content_list, key=(lambda x: x['beer_date']))
+    elif align_type == 2:   # 오래된 상품순
+        content_list = sorted(content_list, key=(lambda x: x['beer_date']), reverse=True)
+    elif align_type == 3:   # 1개 가격 낮은순
+        content_list = sorted(content_list, key=(lambda x: x['one_min']))
+    elif align_type == 4:   # 1개 가격 높은순
+        content_list = sorted(content_list, key=(lambda x: x['one_min']), reverse=True)
+    elif align_type == 5:   # 4개 가격 낮은순
+        content_list = sorted(content_list, key=(lambda x: x['four_min']))
+    elif align_type == 6:   # 4개 가격 높은순
+        content_list = sorted(content_list, key=(lambda x: x['four_min']), reverse=True)
+    elif align_type == 7:   # 별점 높은순
+        content_list = sorted(content_list, key=(lambda x: x['star_point']), reverse=True)
+    elif align_type == 8:   # 별점 낮은순
+        content_list = sorted(content_list, key=(lambda x: x['star_point']))
+    elif align_type == 9:   #  랜덤
+        random.shuffle(content_list)
+
+    return render_template('index.html', content_list=content_list, isLogin=isLogin)
+
+
+# @app.route('/api/index', methods=['GET'])
+# def check_login():
+#     token_receive = request.cookies.get('mytoken')
+#     if token_receive is not None:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+#         userinfo = db.users.find_one({'id': payload['id']}, {'_id': 0})
+#         return jsonify({'id': userinfo['id']})
+#     else:
+#         return jsonify({'fail':'fail'})
+
+
+
 
 @app.route('/api/writing', methods=['POST'])
 def save_beer():
+    def checking(a):
+        print(a)
+        if a is "":
+            a=0
+            print("빈 문자열 발견")
+        return (a)
     content_list = list(db.content.find({}, {'_id': False}))
     beer_num = len(content_list)+1
 
@@ -54,24 +114,35 @@ def save_beer():
     beer_type = request.form['beer_type']
     beer_company= request.form['beer_company']
     beer_date= request.form['beer_date']
+    beer_country=request.form['beer_country']
     #가격정보 전부 가져오기
-    mini_price_1 = request.form['mini_price_1']
-    mini_price_4 = request.form['mini_price_1']
-    gs_price_1 = request.form['gs_price_1']
-    gs_price_4 = request.form['gs_price_4']
-    cu_price_1 = request.form['cu_price_1']
-    cu_price_4 = request.form['cu_price_4']
-    seven_price_1 = request.form['seven_price_1']
-    seven_price_4 = request.form['seven_price_4']
-    nobrand_price_1 = request.form['nobrand_price_1']
-    nobrand_price_4 = request.form['nobrand_price_4']
 
-    dic_temp = [{'store':'CU'},{'one':cu_price_1},{'four':cu_price_4},{'store':'mini'},{'one':mini_price_1},{'four':mini_price_4},{'store':'gs'},{'one':gs_price_1},{'four':gs_price_4},{'store':'nobrand'},{'one':nobrand_price_1},{'four':nobrand_price_4},{'store':'seven'},{'one':seven_price_1},{'four':seven_price_4}]
+    mini_price_1 = int(checking(request.form['mini_price_1']))
+    mini_price_4 = int(checking(request.form['mini_price_4']))
+    gs_price_1 = int(checking(request.form['gs_price_1']))
+    gs_price_4 = int(checking(request.form['gs_price_4']))
+    cu_price_1 = int(checking(request.form['cu_price_1']))
+    cu_price_4 = int(checking(request.form['cu_price_4']))
+    seven_price_1 = int(checking(request.form['seven_price_1']))
+    seven_price_4 = int(checking(request.form['seven_price_4']))
+    nobrand_price_1 = int(checking(request.form['nobrand_price_1']))
+    nobrand_price_4 = int(checking(request.form['nobrand_price_4']))
+    dic_temp=[]
+    if cu_price_1 != 0:
+        dic_temp.append({'store': 'CU', 'one': cu_price_1, 'four':cu_price_4})
+    if gs_price_1 != 0:
+        dic_temp.append({'store': 'gs', 'one': gs_price_1, 'four':gs_price_4})
+    if seven_price_1 != 0:
+        dic_temp.append({'store': 'seven', 'one': seven_price_1, 'four':seven_price_4})
+    if mini_price_1 != 0:
+        dic_temp.append({'store': 'mini', 'one': mini_price_1, 'four':mini_price_4})
+    if nobrand_price_1 != 0:
+        dic_temp.append({'store': 'nobrand', 'one': nobrand_price_1, 'four':nobrand_price_4})
 
 
     #밑쪽으로는 파일 저장하기
     file = request.files["file_give"]
-    today = datetime.now()
+    today = datetime.now() #원래 dateime.now()
     mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
     extantion = file.filename.split('.')[-1]
     filename = f'file-{mytime}'
@@ -85,11 +156,11 @@ def save_beer():
         'beer_type':beer_type,
         'beer_company':beer_company,
         'beer_date':beer_date,
+        'beer_country': beer_country,
         'file':f'{filename}.{extantion}',
         'price':dic_temp
     }
     db.content.insert_one(doc)
-
     return jsonify({'msg': '새 맥주 등록 완료'})
 
 
@@ -168,6 +239,7 @@ def post_review():
 @app.route('/remove/review', methods=["POST"])
 def delete_review():
     review_num = request.form["review_num"]
+
     print(review_num)
 
     # db.review.update_one({'review_num': int(review_num)}, {'$set': {'deleted': 1}})
@@ -177,7 +249,6 @@ def delete_review():
     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
     userinfo = db.users.find_one({'id': payload['id']}, {'_id': 0})
     writer = db.review.find_one({'id':userinfo['id']})
-
 
     if userinfo['id'] == writer['id']:
         db.review.update_one({'review_num': int(review_num)}, {'$set': {'deleted': 1}})
@@ -205,11 +276,16 @@ def api_valid():
         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
 
+
 @app.route('/login')
 def login():
     msg = request.args.get("msg")
     return render_template('login.html', msg=msg)
 
+@app.route('/logout')
+def logout():
+    msg = request.args.get("msg")
+    return render_template('logout.html', msg=msg)
 
 @app.route('/register')
 def register():
@@ -259,7 +335,8 @@ def api_login():
         payload = {
             'id': id_receive,
             # 'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60 * 60 * 24)
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+            #'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)
+            'exp': datetime.utcnow() + timedelta(seconds=10)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
         #token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
@@ -269,15 +346,13 @@ def api_login():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-    # #회원가입시 아이디 중복체크
-    # @app.route('/api/register', methods=['POST'])
-    # def check_id():
-    #     id_receive = request.form['id_give']
-    #     user = db.users.find_one({'id': id_receive})
-    #     if user is not None:  # 유저있으면
-    #         return jsonify({'msg': '아이디 사용 불가'})
-    #     else:
-    #         return jsonify({'msg': '아이디 사용 가능'})
+
+#아이디 중복 레크
+@app.route('/register/check_dup', methods=['POST'])
+def check_dup():
+    userid_receive = request.form['userid_give']
+    exists = bool(db.users.find_one({"id": userid_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
 
 
